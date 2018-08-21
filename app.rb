48404360d58fe75fcabdb9f57ed79065d311143c
parasquid <tristan.gomez@gmail.com>
2018-08-21 23:22:40 +0800

    initial commit

---
+++ app.rb
@@ -0,0 +1,15 @@
+require "sinatra"
+require "base64"
+require "json"
+
+get "/" do
+  erb :"index.html"
+end
+
+get "/pixel.png" do
+  puts JSON.pretty_generate(request.cookies)
+
+  # this renders the actual 1x1 transparent pixel (hence the name pixel tracker)
+  content_type "image/png"
+  Base64.decode64("iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8/5+hHgAHggJ/PchI7wAAAABJRU5ErkJggg==")
+end
