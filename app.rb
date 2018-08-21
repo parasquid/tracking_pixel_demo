15318f3a887185e268b0a4d3659d157fd2a5c7d4
parasquid <tristan.gomez@gmail.com>
2018-08-21 23:45:57 +0800

    add example for iframe pixel tracker

--- app.rb
+++ app.rb
@@ -6,6 +6,7 @@ get "/" do
   erb :"index.html"
 end
 
+# this is the classic "pixel tracker" where the data is processed in the backend
 get "/pixel.png" do
   puts "query string: #{request.query_string}"
   puts "cookies: #{JSON.pretty_generate(request.cookies)}"
@@ -13,4 +14,19 @@ get "/pixel.png" do
   # this renders the actual 1x1 transparent pixel (hence the name pixel tracker)
   content_type "image/png"
   Base64.decode64("iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8/5+hHgAHggJ/PchI7wAAAABJRU5ErkJggg==")
+end
+
+# this is an iframe tracker which is more flexible, but requires the browser to execute js
+get "/iframe" do
+  content_type "text/html"
+  %|<html>
+      <head>
+        <script>
+          console.log("hello, world");
+          console.log("query string: #{request.query_string}")
+          console.log(`cookies: #{JSON.pretty_generate(request.cookies)}`)
+        </script>
+      </head>
+    </html>
+  |
 end
